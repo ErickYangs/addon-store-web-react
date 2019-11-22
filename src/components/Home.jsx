2348213f1ba5a962c6f795b192893ec1bddeefa1
ericyang <ericyangchen@foxmail.com>
2019-11-22 21:22:14 +0800

    add react-router-dom

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
+  render() {
+    return <div>Home Component</div>
+  }
+}
+
+export default Home
