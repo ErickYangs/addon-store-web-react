77da99a26eeb112a83035702a6c7b1b41a11bea9
ericyang <ericyangchen@foxmail.com>
2019-11-25 09:35:21 +0800

    code

--- src/components/accountModule/Login.jsx
+++ src/components/accountModule/Login.jsx
@@ -1,8 +1,30 @@
 import React, { Component } from 'react'
+import loginScss from '../../styles/login.module.scss'
 
 export class LoginLayout extends Component {
   render() {
-    return <div></div>
+    console.log(loginScss)
+    return (
+      <div>
+        <div className={loginScss.login_layout}>
+          <div className={loginScss.title}></div>
+          <div className="tips _tps_top">
+            Already have Account.
+            <span>Sign up.</span>
+          </div>
+          <div className={loginScss.qrcode}>
+            <img src="imgUrl" alt="true" />
+          </div>
+          <div className="tips _tps_btm">
+            Log in using
+            <span>Authenticator</span> scan code
+          </div>
+          <div className={loginScss.download}>
+            <span className="link">Download Authenticator App</span>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
